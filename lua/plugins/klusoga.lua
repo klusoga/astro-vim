d5e0ab23a36c5f71a88f0a92fe63a08cc4ebe6d5
Florian Kludaß <florian.kludass@rohde-schwarz.com>
2025-02-12 10:08:26 +0100

    set catpuccin theme

--- lua/plugins/klusoga.lua
+++ lua/plugins/klusoga.lua
@@ -6,15 +6,28 @@ return {
       mappings = {
         n = {
           ["<TAB>"] = { "<cmd>bnext<Cr>", desc = "Switch tab to right" },
-          ["<S-TAB>"] = { "<cmd>bNext<Cr>", desc = "Switch tab to left" }
+          ["<S-TAB>"] = { "<cmd>bNext<Cr>", desc = "Switch tab to left" },
         },
         t = {
-          ["<esc>"] = { [[<C-\><C-n>v]], desc = "Terminal to visual mode" }
+          ["<F6>"] = { [[<C-\><C-n>v]], desc = "Terminal to visual mode" },
         },
         v = {
           ["a"] = { "<cmd>startinsert<CR>", desc = "Visual to terminal mode" },
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function(plugin, opts)
+      -- run default AstroNvim nvim-dap-ui configuration function
+      require "astronvim.plugins.configs.nvim-dap-ui"(plugin, opts)
+
+      -- disable dap events that are created
+      local dap = require "dap"
+      dap.listeners.after.event_initialized.dapui_config = nil
+      dap.listeners.before.event_terminated.dapui_config = nil
+      dap.listeners.before.event_exited.dapui_config = nil
+    end,
+  },
 }
